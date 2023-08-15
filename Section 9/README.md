47f3c6eb22d4a88ec2e23338fb49a5e85c32d34d
Josh <programmmerjosh@gmail.com>
2023-08-15 17:37:15 +0100

    new section. starting with scoped style tags

---
+++ Section 9/README.md
@@ -0,0 +1,7 @@
+### Index
+
+## Diving Deeper Into Components
+
+> cmp-adv-01-starting-setup
+>
+> > scoped style tags
---
+++ Section 9/cmp-adv-01-starting-setup/src/components/TheHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <h1>More on Vue Components</h1>
+  </header>
+</template>
+
+<!-- notice the scoped attribute on the style tag -->
+<!-- this attribute restricts the styling defined here to this file only -->
+<style scoped>
+  header {
+    width: 100%;
+    height: 5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #14005e;
+  }
+
+  header h1 {
+    color: white;
+    margin: 0;
+  }
+</style>
