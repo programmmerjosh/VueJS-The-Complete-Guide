906043c3eea2de91bb5784422e82d9abccf57304
Josh <programmmerjosh@gmail.com>
2023-09-04 12:44:40 +0100

    add base card styling and header

--- Section 10 - Course Project/The-Learning-Resources-App/src/App.vue
+++ Section 10 - Course Project/The-Learning-Resources-App/src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <!-- exporting the resources into it's own file (and component) to keep things leaner here -->
+  <the-header title="RememberMe"></the-header>
   <stored-resources :resources="storedResources"></stored-resources>
 </template>
+å
 
 <script>
 import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
 
 export default {
   components: {
     StoredResources,
+    TheHeader,
   },
   data() {
     return {
